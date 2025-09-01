c33e0b3415e4b5083802ecc51e1a53b9110d1860
Eric-Philippe <ericphlpp@gmail.com>
2025-09-01 12:48:34 +0200

    Add grafana dashboard and URLs for Swagger

--- src/main/kotlin/org/ericp/ehub/server/config/OpenApiConfig.kt
+++ src/main/kotlin/org/ericp/ehub/server/config/OpenApiConfig.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -20,6 +21,7 @@ class OpenApiConfig {
                     .version("1.0.0")
                     .description("Personal utilities hub API - All endpoints require API key authentication")
             )
+            .servers(listOf(Server().url("https://ehub.homeserver-ericp.fr"), Server().url("http://localhost:8080")))
             .components(
                 Components()
                     .addSecuritySchemes("apiKey",
